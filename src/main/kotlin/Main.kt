168c9668786f9ab84a21cba65e9a7bc0098c7278
WallisonFer <wallisonsp.santos@gmail.com>
2023-02-07 00:20:23 -0300

    Implementando função transferência

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ fun main() {
     println("Bem vindo ao ByteBank")
     val contaAlex = Conta()
     contaAlex.titular = "Alex"
+    contaAlex.numero = 2002
+    contaAlex.saldo = 550.0
     println(contaAlex.titular)
 
     val contaFran = Conta()
@@ -11,17 +13,75 @@ fun main() {
     println("Titular: ${contaFran.titular}")
     println("Numero da conta: ${contaFran.numero}")
     println("Saldo: ${contaFran.saldo}")
+
+    println("Depositando na conta da Fran")
+    contaFran.deposita(valor = 50.0)
+    println(contaFran.saldo)
+
+    println("Sacando na conta da Fran")
+    contaFran.saque(valor = 50.0)
+    println(contaFran.saldo)
+
+    println("Transferindo valores de Fran para Alex")
+
+    if(contaFran.transfere(valor = 100.0, contaAlex)){
+        println("Tranferência realizada com sucesso!")
+    }else{
+        println("Falha na transferência")
+    }
+    println("SALDO ALEX ${contaAlex.saldo}")
+    println("SALDO FRAN ${contaFran.saldo}")
 }
 
-class Conta{
+
+class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+
+    fun saque(valor: Double) {
+        if (this.saldo >= valor) {
+            saldo -= valor
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.saldo += valor
+            return true
+        }
+        return false
+        }
+
+    }
+
+
+
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("NumeroX: $numeroX")
+    println("NumeroY: $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "João"
+    var contaMaria = contaJoao
+    contaMaria.titular = "Maria"
+
+    println("Titular conta joão: ${contaJoao.titular}")
+    println("Titular conta Maria: ${contaMaria.titular}")
 }
 
-fun testaLacos(){
+fun testaLacos() {
     var i = 0
-    while(i < 5){
+    while (i < 5) {
         val titular = "Alex $i"
         val numeroConta = 1000 * i
         val saldo = i + 10.0
@@ -53,7 +113,8 @@ fun testaLacos(){
 //        testaCondicoes(saldo)
 //        }
 }
-fun testaCondicoes(saldo:Double){
+
+fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0.0 -> println("Conta está positiva!")
         saldo == 0.0 -> println("Conta está neutra")
